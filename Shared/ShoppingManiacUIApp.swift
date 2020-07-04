f87dc784a4ace82987c59b509c0b92d0f3dad482
Dmitry Matyushkin <dmatushkin@gmail.com>
2020-07-04 21:34:11 +0400

    Initial Commit

---
+++ Shared/ShoppingManiacUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingManiacUIApp.swift
+//  Shared
+//
+//  Created by Dmitry Matyushkin on 7/4/20.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingManiacUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
